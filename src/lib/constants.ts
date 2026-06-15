1759c995e284b5585ab1625bcbfef31a379de44e
said-yousuf <saidyousuf.said@gmail.com>
2026-06-15 14:10:37 +0430

    Serve desktop installer from website

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,2 @@
 export const APP_URL = "https://app.crablock.cloud";
-export const DOWNLOAD_URL =
-  "https://github.com/SohailRaoufi/Crablock-cli/releases/download/v0.1.3/Crablock.Desktop_0.1.3_x64-setup.exe";
+export const DOWNLOAD_URL = "/downloads/crablock-desktop-0.1.3-x64-setup.exe";
